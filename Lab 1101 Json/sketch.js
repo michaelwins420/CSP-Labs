fc101315e34e4ee4c3cd9fc026d09dc384b28678
michaelwins420 <43476753+michaelwins420@users.noreply.github.com>
2018-11-01 18:09:58 -0700

    Add files via upload

---
+++ Lab 1101 Json/sketch.js
@@ -0,0 +1,15 @@
+//Michael Costantini
+
+var data;
+var barHeight
+
+function preload(){
+  data = loadJSON("Teams.json");
+  console.log(data);
+
+}
+
+
+function setup(){
+  data.unitedstates[4].teams
+}
