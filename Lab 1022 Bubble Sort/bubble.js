18d0e756d2df92e1204ed3223ace1faa348cbc64
michaelwins420 <43476753+michaelwins420@users.noreply.github.com>
2018-11-05 16:47:40 -0800

    Update bubble.js

--- Lab 1022 Bubble Sort/bubble.js
+++ Lab 1022 Bubble Sort/bubble.js
@@ -1,14 +1,21 @@
+//Michael Costantini
+
+
+var txt;
+
+
+	function preLoad(){
+		txt = loadStrings("words.txt")
+	}
+
 function setup(){//setup function
 	var cnv = createCanvas(800, 800); //canvas variable
 	background(20, 20, 20); //creates background color
 	cnv.position((windowWidth-width)/2, 30); //canvas position
 	fill(200, 30, 150);
+	preload();
+	bubbleSort(txt);
 
-	words = [];
-
-	function preLoad(){
-		var txt = loadStrings('words.txt')
-	}
 
 	console.log(words);
 	for(var i = words.length - 1; i>= 1; i--) {
