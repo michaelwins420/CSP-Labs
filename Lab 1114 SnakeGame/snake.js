946f2c339c1fd0713c24ac49fb1d6b9606a0b137
michaelwins420 <43476753+michaelwins420@users.noreply.github.com>
2018-11-14 22:24:28 -0800

    Add files via upload

---
+++ Lab 1114 SnakeGame/snake.js
@@ -0,0 +1,23 @@
+function Snake(loc, vel){
+  this.loc = loc;
+  this.vel = vel;
+
+  this.run = function(){
+    this.update();
+    this.render();
+  }
+
+
+  this.update = function(){
+    this.loc.add(this.vel);
+    this.loc.x = constrain(this.loc.x, 0, width-w);
+    this.loc.y = constrain(this.loc.y, 0, height-w);
+  }
+
+  this.render = function() {
+    fill(0, 255, 0);
+    rect(this.loc.x, this.loc.y, w, w);
+  }
+
+
+}
